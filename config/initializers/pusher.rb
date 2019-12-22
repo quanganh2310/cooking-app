ae15b0730a3fa6d7e5b3bf0dbb8551b2d8b1be68
nguyenduong2612 <nguyenduong261298@gmail.com>
2019-12-22 23:20:23 +0700

    fix pusher

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,18 +1,5 @@
 require 'pusher'
 
-# pusher = Pusher::Client.new(
-#     app_id: '914360',
-#     key: '2fdd4bc794d49256edb0',
-#     secret: 'c72bac4a2f44e76d5af4',
-#     cluster: 'ap1',
-#     encrypted: true
-# )
-
-# pusher.trigger('my-channel', 'my-event', {
-#   message: 'hello world'
-# })
-
-
 Pusher.app_id = '914360'
 Pusher.key = '2fdd4bc794d49256edb0'
 Pusher.secret = 'c72bac4a2f44e76d5af4'
